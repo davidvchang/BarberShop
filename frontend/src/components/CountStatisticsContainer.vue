f065624022bf1b98c47d0938a47897cf5bc25495
davidvchang <dvalenzuelachang@gmail.com>
2025-01-20 01:06:56 -0700

    Se realizó la sección de las estadisticas, en AdminPage - Versión PC

---
+++ frontend/src/components/CountStatisticsContainer.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    import { Calendar } from 'lucide-vue-next';
+
+    defineProps<{
+        title: string,
+        number: number
+    }>()
+</script>
+
+<template>
+    <div class="flex bg-white rounded-md p-5 shadow-md gap-5">
+        <Calendar class="w-12 h-12"/>
+
+        <div class="flex flex-col">
+            <span class="text-slate-500 text-sm font-semibold">{{ title }}</span>
+            <span class="text-2xl font-semibold">{{ number }}</span>
+        </div>
+    </div>
+</template>
--- frontend/src/views/AdminPage.vue
+++ frontend/src/views/AdminPage.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
   import { Scissors } from 'lucide-vue-next'
+  import CountStatisticsContainer from '../components/CountStatisticsContainer.vue'
 </script>
 
+
 <template>
-  <div class="flex flex-col bg-BGPrincipal h-dvh">
+  <div class="flex flex-col bg-BGPrincipal h-dvh gap-10">
 
     <!-- NAVBAR -->
     <div class="flex w-full h-14 bg-white shadow-md items-center p-3 gap-2">
       <Scissors class="w-8 h-8"/>
       <h1 class="text-2xl font-semibold">BarberShop</h1>
     </div>
+
+    <div class="px-10">
+
+      <div class="flex w-full justify-between">
+        <CountStatisticsContainer title="Total de Citas" number="8"/>
+        <CountStatisticsContainer title="Citas Para Hoy" number="2"/>
+        <CountStatisticsContainer title="Clientes Totales" number="23"/>
+      </div>
+    </div>
+
   </div>
 </template>
