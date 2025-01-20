8e1daa813f5cb41bd770aaa86fea2b1eade563e0
davidvchang <dvalenzuelachang@gmail.com>
2025-01-20 00:42:41 -0700

    Se realizó el NavBar de AdminPage - Version PC

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import AdminPage from './views/AdminPage.vue'
+</script>
+
+<template>
+  <main>
+      <AdminPage/>
+  </main>
+</template>
---
+++ frontend/src/views/AdminPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { Scissors } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="flex flex-col bg-BGPrincipal h-dvh">
+
+    <!-- NAVBAR -->
+    <div class="flex w-full h-14 bg-white shadow-md items-center p-3 gap-2">
+      <Scissors class="w-8 h-8"/>
+      <h1 class="text-2xl font-semibold">BarberShop</h1>
+    </div>
+  </div>
+</template>
