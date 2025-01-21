56ff9f633240ce29f15e3f7be13a880c40fae2cc
davidvchang <dvalenzuelachang@gmail.com>
2025-01-20 23:30:31 -0700

    Se realizó que se abra y cierre el menú lateral - Versión PC

--- frontend/src/views/AdminPage.vue
+++ frontend/src/views/AdminPage.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
   import { Scissors } from 'lucide-vue-next'
-  import { Users, Calendar } from 'lucide-vue-next';
+  import { ref } from 'vue';
+
+  import { Users, Calendar, SquareChevronLeft, Menu, SquareX } from 'lucide-vue-next';
+
   import CountStatisticsContainer from '../components/CountStatisticsContainer.vue'
   import TableTodayAppointments from '../components/TableTodayAppointments.vue'
+  import LateralMenu from '../components/LateralMenu.vue'
+
+  const toggleMenu = ref(false)
+
+  const isOpeonMenu = () => {
+    toggleMenu.value = !toggleMenu.value;
+  }
 </script>
 
 
@@ -10,11 +20,24 @@
   <div class="flex flex-col bg-BGPrincipal h-dvh gap-10">
 
     <!-- NAVBAR -->
-    <div class="flex w-full h-14 bg-white shadow-md items-center p-3 gap-2">
-      <Scissors class="w-8 h-8 text-blue-600"/>
-      <h1 class="text-2xl font-semibold">BarberShop</h1>
-    </div>
+    <div class="flex justify-between w-full h-14 bg-white shadow-md items-center p-3 gap-2">
+      <div class="flex">
+        <Scissors class="w-8 h-8 text-blue-600"/>
+        <h1 class="text-2xl font-semibold">BarberShop</h1>
+      </div>
 
+      <div v-if="toggleMenu === false">
+        <Menu class="w-8 h-8 fixed right-3 top-3  hover:text-blue-400 hover:cursor-pointer hover:transition-all duration-300" @click="isOpeonMenu"/>
+      </div>
+      <div v-if="toggleMenu === true">
+        <SquareX class="w-8 h-8 fixed right-3 top-3 rotate-180  hover:text-blue-400 hover:cursor-pointer hover:transition-all duration-300" @click="isOpeonMenu"/>
+      </div>
+    
+      <div v-if="toggleMenu === true">
+          <LateralMenu/>
+      </div>
+    </div>
+    
     <div class="px-10 flex flex-col gap-10">
 
       <div class="flex w-full justify-between gap-10">
