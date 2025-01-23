7a1116e7610b15df1abea05308de015741ed6d71
davidvchang <dvalenzuelachang@gmail.com>
2025-01-23 00:22:58 -0700

    Se realizó el poder agregar servicios en el frontend

--- frontend/src/views/AddService.vue
+++ frontend/src/views/AddService.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
+    import axios from "axios";
+    import { reactive } from "vue";
 
-defineProps<{
-    closeModal: () => void
-}>()
+    const urlAPI: string = import.meta.env.VUE_APP_POST_SERVICE;
+
+    defineProps<{
+        closeModal: () => void
+    }>()
+
+    interface DataForm{
+        name: string,
+        price: number | null,
+        description: string
+    }
+
+    const formData = reactive<DataForm>({
+        name: "",
+        price: null,
+        description: ""
+    });
+
+    const handleSubmit = async () => {
+        console.log("API URL:", urlAPI);
+        const res = await axios.post(urlAPI, formData)
+        console.log("res: ", res)
+    }
 </script>
 
 <template>
     <div class="w-96 absolute top-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 border shadow-lg rounded-xl">
-        <form class="space-y-4">
+        <form class="space-y-4" @submit.prevent="handleSubmit">
             <span class="text-2xl font-medium flex text-center w-full justify-center">Add Service</span>
             
             <div>
@@ -15,6 +37,7 @@ defineProps<{
                 <input 
                 type="text" 
                 id="service-name" 
+                v-model="formData.name"
                 class="mt-1 block w-full py-2 px-5 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 text-sm"
                 placeholder="Enter service name"
                 />
@@ -25,6 +48,7 @@ defineProps<{
                 <input 
                 type="number" 
                 id="price" 
+                v-model="formData.price"
                 class="mt-1 block w-full py-2 px-5 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 text-sm"
                 placeholder="Enter price"
                 />
@@ -35,6 +59,7 @@ defineProps<{
                 <textarea 
                 id="description" 
                 rows="4"
+                v-model="formData.description"
                 class="mt-1 block w-full py-2 px-5 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 text-sm"
                 placeholder="Enter service description"
                 ></textarea>
@@ -51,8 +76,8 @@ defineProps<{
                 <button 
                 type="submit" 
                 class="w-full py-2 px-4 bg-blue-500 text-white font-medium text-sm rounded-md shadow-sm hover:bg-blue-600 hover:transition duration-300"
-                >
-                Add Service
+                >   
+                Guardar
                 </button>
             </div>
         </form>
