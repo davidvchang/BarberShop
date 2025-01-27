25ddbe1fd452713eeb3e9dcd0f2e87606caa9002
davidvchang <dvalenzuelachang@gmail.com>
2025-01-26 17:28:04 -0700

    Se realizó el diseño del login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -10,7 +10,7 @@
             <div>
                 <span>Password</span>
                 <input type="password" name="password" id="password" class="mt-1 block w-full py-2 px-5 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 text-sm">
-                <p class="text-sm">Don´t you have an account? <a href="" class="text-blue-500 font-medium underline">Sign in</a></p>
+                <p class="text-sm">Don´t have an account? <a href="" class="text-blue-500 font-medium underline">Sign in</a></p>
             </div>
 
             <button 
